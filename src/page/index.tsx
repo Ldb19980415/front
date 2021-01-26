8f1cdd86b914f91f385734f898a7ec17cb1e3370
LAPTOP-7IDAQAQQ\jzyz-ldb <dbliu@kingdomai.com>
2021-01-26 12:05:11 +0800

    构建项目初始代码

---
+++ src/page/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { isExpressionWithTypeArguments } from 'typescript'
+
+
+export default class Page extends React.Component {
+    render () {
+        return <div>kjsal</div>
+    }
+}
